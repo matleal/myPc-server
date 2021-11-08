41e43d5da158435d2c4c93feb4ec79d2d3a5d268
matleal <matcod2@gmail.com>
2021-11-08 11:56:01 -0300

    Estruturação do projeto

---
+++ backend/src/config/vars.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+// import .env variables
+require('dotenv-safe').config({
+  path: path.join(__dirname, '../../.env'),
+  example: path.join(__dirname, '../../.env.example'),
+});
+
+module.exports = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
+  mongo: {
+    uri: process.env.NODE_ENV === 'test' ? process.envcle.MONGO_URI_TESTS : process.env.MONGO_URI,
+  },
+};
