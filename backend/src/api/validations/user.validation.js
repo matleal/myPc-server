48bb67ec92ece9499bc53a97acfe44745e647600
matleal <matcod2@gmail.com>
2021-12-16 14:13:35 -0300

    Removed some validations from user validation

--- backend/src/api/validations/user.validation.js
+++ backend/src/api/validations/user.validation.js
@@ -28,7 +28,6 @@ module.exports = {
   replaceUser: {
     body: {
       email: Joi.string().email().required(),
-      password: Joi.string().min(6).max(128).required(),
       name: Joi.string().max(128),
       role: Joi.string().valid(User.roles),
     },
